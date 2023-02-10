263a6ae74eabcc1d183e8fe6f2264d108a73f92c
kariuantony <kariuantony@gmail.com>
2023-02-10 16:16:11 +0300

    Adds session

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
-import express, { Express, NextFunction, Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import userRouter from "./components/user/user.routes.js";
+import session from "express-session";
 dotenv.config();
 
 const app = express();
-const port: String = process.env.PORT;
+const port: string = process.env.PORT;
+const secret: string = process.env.SESSION_SECRET;
+const expiry: number = 1000 * 60 * 60 * 24 * 7;
 
+const sessionMiddleware = session({
+  secret: secret,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { 
+    secure: false,
+    maxAge: expiry
+  }
+})
+
+app.set('trust proxy', 1)
+app.use(sessionMiddleware)
 
 app.get("/", async (req: Request, res: Response) => {
   res.send("Hello world!");
